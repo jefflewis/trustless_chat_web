805c4fa66e2791169cc38d1faad0f5d32b44ad68
Ben Cornelis <bcornelis@xoi.io>
2021-12-07 16:51:45 -0600

    adds peer js prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import { connectToPeer, BROKERING_ID} from './peer'
+
 export default function App() {
+  const [peerId, setPeerId] = useState('')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>Brokering ID: {BROKERING_ID}</div>
+        <input type="text" onChange={(e) => setPeerId(e.target.value)} />
+        <button onClick={() => { connectToPeer(peerId) }}>Connect to peer</button>
         <p>
           Trustless Chat
         </p>
