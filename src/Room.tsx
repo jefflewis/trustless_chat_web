51a30e0cab6fe628f716c05c50678866c42ebf93
Jeff Lewis <mail@jeffreylewis.me>
2021-12-08 09:36:54 -0600

    Get data working again?

--- src/Room.tsx
+++ src/Room.tsx
@@ -18,7 +18,7 @@ export function Room() {
   useEffect(() => {
     console.log("Starting", { roomId, peer: roomClient._peer });
 
-    if (roomId && !roomClient._peer) {
+    if (roomId) {
       if (localStorage.getItem("roomId")) {
         roomClient.createRoom();
       } else {
@@ -45,7 +45,7 @@ export function Room() {
         <h2>Connected</h2>
         <ul>
           <li>{user}</li>
-          {/* <li>{roomClient._conn?.metadata.name}</li> */}
+          <li>{roomClient._conn?.metadata.name}</li>
         </ul>
       </header>
     </div>
