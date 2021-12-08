fdf0dbbe89d975b78dc8acce647138cdf46d2749
Ben Cornelis <bcornelis@xoi.io>
2021-12-07 19:41:34 -0600

    add stream sending to room client

--- src/room.ts
+++ src/room.ts
@@ -5,26 +5,35 @@ type Message = {
   text: string;
 };
 
-class RoomClient {
-  peer: Peer | null = null;
-  conn: Peer.DataConnection | null = null;
+type Stream = {
+  mediaStream: MediaStream;
+};
 
-  _msgEmitter: EventEmitter;
+class RoomClient {
+  _peer: Peer | null = null;
+  _conn: Peer.DataConnection | null = null;
+  _emitter: EventEmitter;
+  _missedCalls: Peer.MediaConnection[] = [];
 
   constructor() {
-    this._msgEmitter = new EventEmitter();
+    this._emitter = new EventEmitter();
   }
 
   createRoom = async () => {
     return new Promise((resolve) => {
-      this.peer = new Peer();
-      this.peer.on("connection", (peerConn) => {
-        this.conn = peerConn;
+      this._peer = new Peer();
+      this._peer.on("connection", (peerConn) => {
+        this._conn = peerConn;
         console.log(`Peer connected`, peerConn);
-        this.conn.on("data", this._receiveMessage);
+        this._conn.on("data", this._receiveMessage);
       });
 
-      this.peer.on("open", (id) => {
+      this._peer.on("call", (call) => {
+        this._missedCalls.push(call);
+        call.on("stream", this._receiveStream);
+      });
+
+      this._peer.on("open", (id) => {
         console.log("Initialized Peer with brokering id", id);
         resolve(id);
       });
@@ -32,32 +41,55 @@ class RoomClient {
   };
 
   joinRoom = (roomId: string) => {
-    if (!this.peer) {
-      this.peer = new Peer();
+    if (!this._peer) {
+      this._peer = new Peer();
     }
-    this.conn = this.peer.connect(roomId);
-    this.conn.on("open", () => {
-      this.conn?.send("I connected!");
+    this._conn = this._peer.connect(roomId);
+    this._conn.on("open", () => {
+      this._conn?.send("I connected!");
     });
 
-    this.conn.on("data", this._receiveMessage);
+    this._conn.on("data", this._receiveMessage);
+  };
+
+  call = (mediaStream: MediaStream) => {
+    if (!this._conn || !this._peer) {
+      throw new Error("no connection for call");
+    }
+    const call = this._peer.call(this._conn.peer, mediaStream);
+    call.on("stream", this._receiveStream);
+  };
+
+  _receiveStream = (mediaStream: MediaStream) => {
+    this._emitter.emit("STREAM", { mediaStream });
+  };
+
+  subcribeToStreams = (subscribe: (stream: Stream) => void) => {
+    this._emitter.on("STREAM", subscribe);
+  };
+
+  answer = (mediaStreams: MediaStream[]) => {
+    mediaStreams.forEach((mediaStream) => {
+      const call = this._missedCalls.pop();
+      call?.answer(mediaStream);
+    });
   };
 
   _receiveMessage = (rawMsg: any) => {
     const msg: Message = JSON.parse(rawMsg);
     console.log("Received", msg);
-    this._msgEmitter.emit("MESSAGE", msg);
+    this._emitter.emit("MESSAGE", msg);
   };
 
   subscribeToMessages = (subscriber: (msg: Message) => void) => {
-    this._msgEmitter.on("MESSAGE", subscriber);
+    this._emitter.on("MESSAGE", subscriber);
   };
 
   sendMessage = (msg: Message) => {
-    if (!this.conn) {
+    if (!this._conn) {
       throw new Error("cant send a message without a connection");
     }
-    this.conn.send(JSON.stringify(msg));
+    this._conn.send(JSON.stringify(msg));
   };
 }
 
